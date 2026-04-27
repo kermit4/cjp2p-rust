5686661edf13afbd247cda67db852d683270b52c
Christopher Pearson <kermit4@gmail.com>
2026-04-27 16:44:55 +0100

    apk -- logs in Downloads/

--- android/app/src/main/java/com/cjp2p/BackendService.java
+++ android/app/src/main/java/com/cjp2p/BackendService.java
@@ -6,6 +6,7 @@ import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -14,6 +15,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class BackendService extends Service {
     private static final String TAG = "cjp2p";
@@ -42,6 +46,8 @@ public class BackendService extends Service {
             try {
                 File filesDir = getFilesDir();
                 extractPublicAssets(filesDir);
+                redirectStderrToFile();
+                android.system.Os.setenv("RUST_LOG", "info", true);
                 NativeLib.start(filesDir.getAbsolutePath());
                 Log.i(TAG, "P2P engine started in-process");
             } catch (Exception e) {
@@ -49,6 +55,25 @@ public class BackendService extends Service {
             }
         }
         return START_STICKY;
+    }
+
+    private void redirectStderrToFile() {
+        try {
+            File logFile = new File(
+                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                "cjp2p.log");
+            FileOutputStream fos = new FileOutputStream(logFile, true);
+            String header = "\n=== " +
+                new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(new Date()) +
+                " ===\n";
+            fos.write(header.getBytes());
+            fos.flush();
+            // Rewire fd 2 (stderr) so env_logger output lands in the file
+            android.system.Os.dup2(fos.getFD(), 2);
+            Log.i(TAG, "Logging to " + logFile.getAbsolutePath());
+        } catch (Exception e) {
+            Log.w(TAG, "Could not open log file: " + e.getMessage());
+        }
     }
 
     private void extractPublicAssets(File filesDir) throws IOException {
