ef5f50a932c177c576d23aa72c1129f8e7ea4f16
Christopher Pearson <kermit4@gmail.com>
2026-05-05 12:55:43 +0100

    stream almost done

--- src/main.rs
+++ src/main.rs
@@ -1951,9 +1951,13 @@ fn handle_web_request(
                             );
                             let mut start: usize = 0;
                             let mut end: usize = 0;
+                            let mut ranged=false;
                             if let Some(range) = req.headers.get("range") {
                                 sscanf!(range, "bytes={}-{}", start, end).ok();
+                info!("got ranged http req {} range {:?}",req.path,range);
+                                ranged=true;
                             }
+                else { info!("got unranged http req {} start/end {} {} {:?} ",req.path,start,end,req.headers); }
                             let cache_path = latest_cache_path(&ed25519.to_string(), &name);
                             let sha256_opt = load_sha256_from_latest_cache(&cache_path);
                             let pending_latest = if !is_local(&stream) {
@@ -1999,6 +2003,7 @@ fn handle_web_request(
                                 id: sha256_opt.unwrap_or_default(),
                                 http_start: start,
                                 http_end: end,
+                                ranged: ranged,
                                 http_socket: stream,
                                 waiting_for_browser: false,
                                 http_done: false,
@@ -2037,12 +2042,23 @@ fn handle_web_request(
                         let peers = ps.best_peers(250, 6);
                         ss.request_blocks(ps, peers);
                     }
+            let mut ranged=false;
+            if let Some(range) = req.headers.get("range") {
+                info!("got ranged http req {} range {:?}",req.path,range);
+                sscanf!(range, "bytes={}-{}",start,end).ok();
+                ranged=true;
+            } else {
+                info!("got unranged http req {} start/end {} {} {:?} ",req.path,start,end,req.headers);
+            }
+
+            info!("http start end {start} {end}");
                     let index = ps.content_gateways.len();
                     ps.content_gateways.push(ContentGateway {
                         id: full_id,
                         http_start: start,
-                        http_end: if end != 0 { end } else { 1 << 62 },
+                        http_end: if end != 0 { end } else { 0x7fffffff },
                         http_socket: stream,
+                        ranged: ranged,
                         waiting_for_browser: false,
                         http_done: false,
                         sent_header: false,
@@ -2073,9 +2089,11 @@ fn handle_web_request(
                 return;
             }
 
+            let mut ranged=false;
             if let Some(range) = req.headers.get("range") {
                 info!("got ranged http req {} range {:?}",req.path,range);
                 sscanf!(range, "bytes={}-{}",start,end).ok();
+                ranged=true;
             } else {
                 info!("got unranged http req {} start/end {} {} {:?} ",req.path,start,end,req.headers);
             }
@@ -2087,6 +2105,7 @@ fn handle_web_request(
                 //                http_time: Instant::now(),
                 http_start: start,
                 http_end: end,
+                ranged: ranged,
                 http_socket: stream,
                 waiting_for_browser: false,
                 http_done: false,
@@ -2930,11 +2949,12 @@ struct ContentGateway {
     ///http_time: Instant,
     http_start: usize,
     http_end: usize,
+    ranged: bool,
     http_socket: TcpStream,
     waiting_for_browser: bool,
     http_done: bool,
     sent_header: bool,
-    eof: usize,
+    eof: Some(usize),
     pending_latest: Option<LatestData>,
 }
 impl ContentGateway {
@@ -2989,14 +3009,25 @@ impl ContentGateway {
     fn serve_mmap(&mut self, mmap: &MmapMut, available_end: usize) {
         if !self.sent_header {
             let mime_type = mimetype_detector::detect(&mmap[0..]);
-            let response = format!(
+            let response = 
+                                if self.ranged {
+                format!(
                                 "HTTP/1.1 206 Partial Content\r\n\
                                  Content-Length: {}\r\n\
                                  Content-Disposition: inline\r\n\
                                  Accept-Range: bytes\r\n\
                                  Content-Range: bytes {}-{}/{}\r\n\
                                  Content-Type: {}\r\n\r\n"
-            ,self.http_end-self.http_start,self.http_start,self.http_end-1, self.eof, mime_type.mime());
+            ,self.http_end-self.http_start,self.http_start,self.http_end-1, self.eof, mime_type.mime())
+                                } else { format!(
+                                "HTTP/1.1 200 OK\r\n\
+                                 Content-Length: {}\r\n\
+                                 Content-Disposition: inline\r\n\
+                                 Accept-Range: bytes\r\n\
+                                 Content-Type: {}\r\n\r\n"
+            ,self.http_end-self.http_start, mime_type.mime())
+                                };  
+            info!("sending http client {}",response);
             match self.http_socket.write_all(response.as_bytes()) {
                 Ok(_) => (),
                 Err(e) => {
@@ -3313,7 +3344,7 @@ fn maintenance(
             i.request_blocks(ps, i.peers.clone()); // resume (un-stall)
         }
         let peers = ps.best_peers(50 * try_harder, 6);
-        info!("searching  {} peers",peers.len());
+        info!("searching  {} peers fo9 {}",peers.len(),i.id);
         i.request_blocks(ps, peers);
         // TODO the longer its been stuck, the more it should be ignored to try others, instead of
         // this pure random
@@ -3337,7 +3368,7 @@ fn maintenance(
                 ss.request_blocks(ps, ss.peers.clone());
             }
             let peers = ps.best_peers(50 * 5, 6);
-            info!("searching {} peers",peers.len());
+            info!("searching  {} peers fo9 {}",peers.len(),ss.id);
             ss.request_blocks(ps, peers);
             // TODO the longer its been stuck, the more it should be ignored to try others, instead of
             // this pure random
